84204f2ba0bb9c29e67e3db2c414f759b7b79940
gusta <gustavobocato99@gmail.com>
2024-09-11 13:19:06 -0300

    Treating parentheses

--- Calculadora.java
+++ Calculadora.java
@@ -1,11 +1,31 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculadora {
+
     public String resolveParenteses(String expressao){
 
-        return "";
-    }
+        Pattern pattern = Pattern.compile("\\([^()]*\\)");
+        Matcher matcher = pattern.matcher(expressao);
+        boolean temParenteses = matcher.find();
 
-    public double avaliaNumero(String expressao){
-        return Double.parseDouble(expressao);
+        if(!temParenteses) return String.valueOf(avaliaSemParenteses(expressao));
+
+        while(temParenteses) {
+
+            String parenteses = matcher.group();
+            String conteudoParenteses = parenteses.substring(1, parenteses.length() - 1);
+            String[] array = expressao.split("\\([^()]*\\)", 2);
+            String subExpressaoEsquerda = array[0];
+            String subExpressaoDireita = array[1];
+
+            expressao = subExpressaoEsquerda + resolveParenteses(conteudoParenteses) + subExpressaoDireita;
+
+            matcher = pattern.matcher(expressao);
+            temParenteses = matcher.find();
+        }
+
+        return expressao;
     }
 
     public double avaliaSoma(String expressao){
@@ -74,20 +94,18 @@ public class Calculadora {
         return resultado;
     }
 
+    public double avaliaNumero(String expressao){
+        return Double.parseDouble(expressao);
+    }
+
+    public double avaliaSemParenteses(String expressao){
+        return avaliaSoma(expressao);
+    }
+
     public void avaliaExpressao(String expressao){
-        double avaliacao;
 
-        try{
+        String expressaoSemParenteses = resolveParenteses(expressao);
 
-            avaliacao = avaliaSoma(expressao);
-
-        }catch (Exception ex){
-
-            System.out.println(ex.getMessage());
-            return;
-
-        }
-
-        System.out.println(avaliacao);
+        System.out.println(avaliaSemParenteses(expressaoSemParenteses));
     }
 }
